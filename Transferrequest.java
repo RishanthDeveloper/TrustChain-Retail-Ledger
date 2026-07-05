0ac91ba0d60ab52bd0e30ef92683d27a1321505a
Rishanth P <rishanth167@gmail.com>
2026-07-05 22:25:41 +0530

    Create Transferrequest.java

---
+++ Transferrequest.java
@@ -0,0 +1,16 @@
+package com.trustchain.backend.dto;
+
+public class TransferRequest {
+    private String newOwnerId;
+    private String newOwnerRole; // WHOLESALER, RETAILER, CONSUMER
+    private String newStatus;    // IN_TRANSIT, DELIVERED, SOLD
+
+    public String getNewOwnerId() { return newOwnerId; }
+    public void setNewOwnerId(String newOwnerId) { this.newOwnerId = newOwnerId; }
+
+    public String getNewOwnerRole() { return newOwnerRole; }
+    public void setNewOwnerRole(String newOwnerRole) { this.newOwnerRole = newOwnerRole; }
+
+    public String getNewStatus() { return newStatus; }
+    public void setNewStatus(String newStatus) { this.newStatus = newStatus; }
+}
